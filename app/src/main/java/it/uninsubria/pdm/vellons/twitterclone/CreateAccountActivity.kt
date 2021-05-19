106b5bd621702ff3edc6d14f311c5352f7cd3e88
vellons <vellons@users.noreply.github.com>
2021-05-20 00:18:07 +0200

    Now user info will be saved in Firestore

--- app/src/main/java/it/uninsubria/pdm/vellons/twitterclone/CreateAccountActivity.kt
+++ app/src/main/java/it/uninsubria/pdm/vellons/twitterclone/CreateAccountActivity.kt
@@ -2,12 +2,16 @@ package it.uninsubria.pdm.vellons.twitterclone
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
 import java.util.regex.Pattern
@@ -15,11 +19,17 @@ import java.util.regex.Pattern
 class CreateAccountActivity : AppCompatActivity() {
     private val TAG = "CreateAccountActivity"
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    var name: String = ""
+    var user: String = ""
+    var email: String = ""
+    var pass: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
         auth = Firebase.auth
+        firestore = Firebase.firestore
     }
 
     fun goBack(v: View) {
@@ -32,10 +42,10 @@ class CreateAccountActivity : AppCompatActivity() {
         val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
         val editTextPassword1 = findViewById<EditText>(R.id.editTextPassword1)
 
-        val name: String = editTextName.text.toString()
-        val user: String = editTextUsername.text.toString()
-        val email: String = editTextEmail.text.toString()
-        val pass: String = editTextPassword1.text.toString()
+        name = editTextName.text.toString()
+        user = editTextUsername.text.toString()
+        email = editTextEmail.text.toString()
+        pass = editTextPassword1.text.toString()
         var errors = 0
 
         if (!isValidName(name)) {
@@ -54,12 +64,8 @@ class CreateAccountActivity : AppCompatActivity() {
             errors += 1
             editTextUsername.error = getString(R.string.invalid_username)
         }
-        if (isValidUsername(user) && !isFreeUsername(user)) {
-            errors += 1
-            editTextUsername.error = getString(R.string.invalid_username_already_used)
-        }
         if (errors == 0) {
-            createAccount(email, pass)
+            checkUsernameAndCreateAccount(user)
         }
     }
 
@@ -68,7 +74,7 @@ class CreateAccountActivity : AppCompatActivity() {
     }
 
     private fun isValidEmail(email: String?): Boolean {
-        if (email == null) return false
+        if (email == null || email.isEmpty()) return false
         val emailPattern =
             ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
         val pattern = Pattern.compile(emailPattern)
@@ -89,10 +95,29 @@ class CreateAccountActivity : AppCompatActivity() {
         return str.length >= 4 && matcher.matches()
     }
 
-    private fun isFreeUsername(str: String?): Boolean {
-        if (str == null) return false
+
+    private fun checkUsernameAndCreateAccount(str: String?) {
+        if (str == null) return
         val lowerUsername = str.toLowerCase(Locale.ROOT)
-        return lowerUsername != "alex"
+        // Check if username already exists in database
+        firestore.collection("users")
+            .whereEqualTo("username", lowerUsername)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    if (document["username"] == lowerUsername) { // Username already exist
+                        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
+                        editTextUsername.error = getString(R.string.invalid_username_already_used)
+                        return@addOnSuccessListener
+                    }
+                }
+                createAccount(email, pass)
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents. Failed to check username", exception)
+                displayToast("Failed to check username")
+            }
     }
 
     private fun createAccount(email: String, password: String) {
@@ -103,16 +128,51 @@ class CreateAccountActivity : AppCompatActivity() {
                     Log.d(TAG, "createUserWithEmail: success")
                     val user = auth.currentUser
                     val uid = user?.uid
-                    Log.d(TAG, "Registered user:" + uid.toString())
+                    if (uid != null) {
+                        Log.d(TAG, "Registered user: $uid. Saving in users collection")
+                        saveUser(uid)
+                    } else {
+                        Log.w(TAG, "Failed to get uid!")
+                    }
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "createUserWithEmail: failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed: " + (task.exception?.message ?: "check logs"),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    displayToast(
+                        "Authentication failed: " + (task.exception?.message ?: " check logs")
+                    )
                 }
             }
+    }
+
+    private fun saveUser(uid: String) {
+        val user = hashMapOf(
+            "username" to user,
+            "verified" to false,
+            "name" to name,
+            "photo" to null,
+            "registeredAt" to FieldValue.serverTimestamp(),
+            "bio" to ""
+        )
+        firestore.collection("users").document(uid)
+            .set(user)
+            .addOnSuccessListener {
+                Log.d(TAG, "User $uid saved to DB")
+                displayToast("YOU ARE IN!")
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error writing document. Failed to save user in collection", exception)
+                displayToast("Failed to save user to DB")
+            }
+    }
+
+
+    private fun displayToast(string: String) {
+        val toast = Toast.makeText(
+            this,
+            string,
+            Toast.LENGTH_SHORT
+        )
+        toast.setGravity(Gravity.TOP, 0, 75)
+        toast.show()
     }
 }
