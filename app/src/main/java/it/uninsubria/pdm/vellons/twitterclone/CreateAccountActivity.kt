0dbdedba49a344e994901f46cd4323ba6234abb3
vellons <vellons@users.noreply.github.com>
2021-05-19 22:50:25 +0200

    Added Firebase auth + account creation

--- app/src/main/java/it/uninsubria/pdm/vellons/twitterclone/CreateAccountActivity.kt
+++ app/src/main/java/it/uninsubria/pdm/vellons/twitterclone/CreateAccountActivity.kt
@@ -1,16 +1,25 @@
 package it.uninsubria.pdm.vellons.twitterclone
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.util.*
 import java.util.regex.Pattern
 
 class CreateAccountActivity : AppCompatActivity() {
+    private val TAG = "CreateAccountActivity"
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        auth = Firebase.auth
     }
 
     fun goBack(v: View) {
@@ -27,21 +36,30 @@ class CreateAccountActivity : AppCompatActivity() {
         val user: String = editTextUsername.text.toString()
         val email: String = editTextEmail.text.toString()
         val pass: String = editTextPassword1.text.toString()
+        var errors = 0
 
         if (!isValidName(name)) {
+            errors += 1
             editTextName.error = getString(R.string.invalid_name)
         }
         if (!isValidEmail(email)) {
+            errors += 1
             editTextEmail.error = getString(R.string.invalid_email)
         }
         if (!isValidPassword(pass)) {
+            errors += 1
             editTextPassword1.error = getString(R.string.invalid_password)
         }
         if (!isValidUsername(user)) {
+            errors += 1
             editTextUsername.error = getString(R.string.invalid_username)
         }
         if (isValidUsername(user) && !isFreeUsername(user)) {
+            errors += 1
             editTextUsername.error = getString(R.string.invalid_username_already_used)
+        }
+        if (errors == 0) {
+            createAccount(email, pass)
         }
     }
 
@@ -75,5 +93,26 @@ class CreateAccountActivity : AppCompatActivity() {
         if (str == null) return false
         val lowerUsername = str.toLowerCase(Locale.ROOT)
         return lowerUsername != "alex"
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail: success")
+                    val user = auth.currentUser
+                    val uid = user?.uid
+                    Log.d(TAG, "Registered user:" + uid.toString())
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail: failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed: " + (task.exception?.message ?: "check logs"),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
--- app/src/main/java/it/uninsubria/pdm/vellons/twitterclone/MainActivity.kt
+++ app/src/main/java/it/uninsubria/pdm/vellons/twitterclone/MainActivity.kt
@@ -6,11 +6,27 @@ import android.view.Gravity
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            // Redirect to navigator if user is logged
+            val intent = Intent(this@MainActivity, NavigatorActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun goToCreateAccount(v: View) {
